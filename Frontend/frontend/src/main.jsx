f245e85ff736b57808301638c7ea084986c97db3
Dhanush-star04 <ramrdhanush2003@gmail.com>
2026-07-27 00:10:34 +0530

    Update login page

--- Frontend/frontend/src/main.jsx
+++ Frontend/frontend/src/main.jsx
@@ -7,7 +7,7 @@ import Fail from './Fail.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Login-page">
     <Routes>
       <Route path='/' element={<App />} ></Route>
       <Route path='/sucess' element={<Sucess />} ></Route>
